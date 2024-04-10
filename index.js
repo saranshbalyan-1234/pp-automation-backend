c55e8a60be384bee980befa21e74157854b54523
Saransh Balyan <saransh.balyan@m2pfintech.com>
2024-04-10 23:10:14 +0530

    test

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ import { setupTimeout, setupRateLimiter, setupCors, setupResponseInterceptor, se
 import { syncDatabase, createSuperAdmin } from "#user/Service/database.js";
 // import { scheduleInit } from "#scheduler/Service/schedulerService.js";
 const app = express();
-
+console.log("saransh",saransh)
 if (process.env.PRINT_ENV == "true") {
     console.debug("======================ENV======================");
     console.debug(process.env);
