7088a0592732b31351dcc14e7c6e0e4843bfb4e9
Saransh Balyan <saransh.balyan@m2pfintech.com>
2024-04-21 15:55:29 +0530

    debug

--- Utils/Database/mongoConnection.js
+++ Utils/Database/mongoConnection.js
@@ -7,7 +7,7 @@ const clientOption = {
    */
   maxPoolSize: 5
 };
-
+mongoose.set('debug', true);
 // CONNECTION EVENTS
 mongoose.connection.on('connected', () => console.log('connected'));
 mongoose.connection.on('open', () => console.log('open'));
@@ -18,24 +18,15 @@ mongoose.connection.on('close', () => console.log('close'));
 
 // If the Node process ends, close the Mongoose connection
 process.on('SIGINT', () => {
-  mongoose.connection.close();
-  console.log('Closed mongo connection');
-  process.exit(0);
+    mongoose.connection.close();
+    console.log('Application crashed');
+    process.exit(0);
 });
 
 export const createDbConnection = DB_URL => {
   try {
-    // Db.on("error", () => console.log("initDbConnection MongoDB Connection Error>> : "));
-
-    /*
-     * Db.once("open", () => {
-     *       mongoose.set('autoIndex', true)
-     *   mongoose.set('autoCreate', true);
-     *   console.log("Success")
-     * });
-     */
     return mongoose.connect(DB_URL, clientOption);
   } catch (error) {
-    console.log('initDbConnection error', error);
+    console.log('Error while connecting to DB', error);
   }
 };
