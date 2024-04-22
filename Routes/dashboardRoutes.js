945b450b43d4fcc457678793b82e31373f8bc31c
Saransh Balyan <saransh.balyan@m2pfintech.com>
2024-04-22 23:47:11 +0530

    removed more code

--- Routes/dashboardRoutes.js
+++ Routes/dashboardRoutes.js
@@ -1,10 +1,8 @@
 import express from 'express';
 
-import { createdReport, dashboard, detailedExecutionReport, executionReport } from '../Controllers/dashboardController.js';
+import { createdReport, dashboard } from '../Controllers/dashboardController.js';
 const Router = express.Router();
 Router.get('/', dashboard);
-Router.post('/detailed-execution-report', detailedExecutionReport);
-Router.post('/execution-report', executionReport);
 Router.post('/created-report', createdReport);
 
 export default Router;
