a4c3eceb8476c6870b3db4d374832550202238b2
Saransh Balyan <saransh.balyan@m2pfintech.com>
2024-05-01 00:01:42 +0530

    removed logs

--- registerRoutes.js
+++ registerRoutes.js
@@ -7,7 +7,6 @@ const getRoutes = (app, type) => {
       console.e('Error', err);
     } else {
       res.forEach(async element => {
-        console.log(res);
 
         const route = await import(element);
         const defaultFile = route.default;
@@ -15,6 +14,7 @@ const getRoutes = (app, type) => {
         const tempAr1 = tempAr[tempAr.length - 4].split('/');
         const name = tempAr1[tempAr1.length - 1];
         app.use(name, defaultFile);
+        
       });
     }
   });
@@ -26,7 +26,7 @@ const registerUnprotectedRoutes = (app) => {
 
 const registerProtectedRoutes = (app) => {
   app.use(validateToken());
-  
+
   getRoutes(app, 'protected.routes');
 };
 
